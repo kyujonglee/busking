ca529ed7a915cae4aa3f2e45dd08fa064d01af95
Koo-Bon-Young <47849386+Koo-Bon-Young@users.noreply.github.com>
2019-05-03 16:19:33 +0900

    Create VO Class 05/03 16:18
    FreeBoard, FreeBoardComment, FreeBoardFile VO 클래스 생성

---
+++ project/buskers/src/main/java/kr/co/buskers/repository/domain/FreeBoardFile.java
@@ -0,0 +1,34 @@
+package kr.co.buskers.repository.domain;
+
+public class FreeBoardFile {
+   private int boardNo;
+   private int fileNo;
+   private String name;
+   private String path;
+   public int getBoardNo() {
+      return boardNo;
+   }
+   public void setBoardNo(int boardNo) {
+      this.boardNo = boardNo;
+   }
+   public int getFileNo() {
+      return fileNo;
+   }
+   public void setFileNo(int fileNo) {
+      this.fileNo = fileNo;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getPath() {
+      return path;
+   }
+   public void setPath(String path) {
+      this.path = path;
+   }
+   
+   
+}
