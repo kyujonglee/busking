7294bc484bca4beb2cd732d767c1d3892d3d599c
이규종 <kyujong93@naver.com>
2019-05-06 23:34:14 +0900

    Update kyu complete agency-check.html 05/06 23:33
    create agency-check html, css

--- js/main/board/agency/side-bar.js
+++ js/main/board/agency/side-bar.js
@@ -1,11 +1,11 @@
 let isSlide = true;
 $(".side-bar__icon:first").click(function(){
     if(!isSlide){
-        $(".side-bar").animate({left : "140px"},"slow");
+        $(".side-bar").animate({left : "140px"},500);
         $(".side-bar").css({"z-index" : 0});
         isSlide = true;
     }else {
-        $(".side-bar").animate({left : "0px"},"slow");
+        $(".side-bar").animate({left : "0px"},500);
         $(".side-bar").css({"z-index" : 1});
         isSlide = false;
     }
