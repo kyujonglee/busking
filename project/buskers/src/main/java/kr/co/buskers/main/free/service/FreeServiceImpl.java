f26578750bd669bd436886133afde3eabaa8ea56
Koo-Bon-Young <47849386+Koo-Bon-Young@users.noreply.github.com>
2019-05-07 17:27:25 +0900

    no message

--- project/buskers/src/main/java/kr/co/buskers/main/free/service/FreeServiceImpl.java
+++ project/buskers/src/main/java/kr/co/buskers/main/free/service/FreeServiceImpl.java
@@ -1,10 +1,24 @@
 package kr.co.buskers.main.free.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.buskers.common.page.FreePageResult;
+import kr.co.buskers.repository.domain.FreePage;
 import kr.co.buskers.repository.mapper.FreeBoardMapper;
 
+@Service
 public class FreeServiceImpl implements FreeService {
 	@Autowired
 	private FreeBoardMapper mapper;
+	
+	public Map<String, Object> list(FreePage freePage) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("list", mapper.selectBoard(freePage));
+		map.put("pageResult", new FreePageResult(freePage.getPageNo(), mapper.selectBoardCount()));
+		return map;
+	}	
 }
