dc3de639c3b66af29c66b561034dba209df53bd7
Koo-Bon-Young <47849386+Koo-Bon-Young@users.noreply.github.com>
2019-05-08 18:51:43 +0900

    no message

--- project/buskers/src/main/java/kr/co/buskers/main/free/controller/FreeBoardController.java
+++ project/buskers/src/main/java/kr/co/buskers/main/free/controller/FreeBoardController.java
@@ -2,10 +2,13 @@ package kr.co.buskers.main.free.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.buskers.main.free.service.FreeService;
 import kr.co.buskers.repository.domain.FreePage;
@@ -26,9 +29,16 @@ public class FreeBoardController {
 		model.addAttribute("pageResult", result.get("pageResult"));
 	}
 	
+	@RequestMapping("list-ajax.do")
+	@ResponseBody
+	public Map<String, Object> sortList(HttpServletRequest request, FreePage freePage) {
+		Map<String, Object> result = service.sortList(request, freePage);
+		
+		return result;
+	}
+	
 	@RequestMapping("detail.do")
 	public void detail(int boardNo, Model model) {
 		model.addAttribute("board", service.detail(boardNo));
-
 	}
 }
