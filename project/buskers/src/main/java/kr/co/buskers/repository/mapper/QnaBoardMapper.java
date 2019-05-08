199ed9332278e69e6e016a99423f8a3c6de355a8
Leehuseung <gntmd4261@naver.com>
2019-05-08 13:09:49 +0900

    Update hs QnaboardDetail 05/08 13:09

--- project/buskers/src/main/java/kr/co/buskers/repository/mapper/QnaBoardMapper.java
+++ project/buskers/src/main/java/kr/co/buskers/repository/mapper/QnaBoardMapper.java
@@ -1,7 +1,13 @@
 package kr.co.buskers.repository.mapper;
 
+import java.util.List;
+
+import kr.co.buskers.repository.domain.FreePage;
 import kr.co.buskers.repository.domain.QnaBoard;
 
 public interface QnaBoardMapper {	
 	public void insertBoard(QnaBoard qnaBoard);
+	List<QnaBoard> selectBoard(FreePage freePage);
+	int selectBoardCount();
+	List<QnaBoard> selectNoticeBoard();
 }
